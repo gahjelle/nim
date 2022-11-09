4127cd797e1c1ae63ca889901da1089a12533176
Geir Arne Hjelle <geirarne@gmail.com>
2022-11-09 12:27:09 +0100

    Create a PyScript site for experimenting with Nim

---
+++ nim.py
@@ -0,0 +1,36 @@
+output_div = Element("output-pane")
+error_div = Element("errors-pane")
+
+antall = 15
+tur = "A"
+
+
+def clear(*args, **kwargs):
+    output_div.element.innerText = ""
+    error_div.element.innerText = ""
+
+
+def nytt_spill():
+    global antall, tur
+    antall = 15
+    tur = "A"
+    clear()
+    info(antall)
+
+
+def info(antall):
+    print(f"Antall: {antall}")
+
+
+def spill(spiller):
+    global antall, tur
+    if antall <= 0:
+        return
+    navn = spiller.__name__.capitalize()
+    valg = max(min(spiller(antall), min(antall, 3)), 1)
+    print(f"{tur}: {navn} tar {valg}")
+    antall -= valg
+    tur = "A" if tur == "B" else "B"
+    info(antall)
+    if antall <= 0:
+        print(f"{navn} tapte")
